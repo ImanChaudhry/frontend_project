98989a1bef7ecd43eab2943a815cd643cfecdbca
Iman Chaudhry <i-man-735@hotmail.com>
2022-06-23 12:46:50 +0100

    Dataloader: added image MajinBuu saga

--- project_backend/src/main/java/com/bnta/dragonball_api/components/Dataloader.java
+++ project_backend/src/main/java/com/bnta/dragonball_api/components/Dataloader.java
@@ -52,7 +52,7 @@ public class Dataloader implements ApplicationRunner {
         Saga cellSaga = new Saga("Cell Saga", Series.DragonBall_Z, "118 - 194", "https://dragonball.guru/wp-content/uploads/2021/03/SeekPng.com_dbz-aura-png_1978081-e1619044254898-768x768.png",
                 LocalDate.of(2000,9,4));
 
-        Saga majinBuuSaga = new Saga("Majin Buu Saga", Series.DragonBall_Z, "195 - 291",
+        Saga majinBuuSaga = new Saga("Majin Buu Saga", Series.DragonBall_Z, "195 - 291", "https://dragonball.guru/wp-content/uploads/2021/03/pngarea.com_kid-buu-png-1410072-e1619048346991.png",
                 LocalDate.of(2001, 9, 10));
 
 
