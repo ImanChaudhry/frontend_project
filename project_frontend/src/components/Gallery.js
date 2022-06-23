ab19b8649b3a9f56df0da10b8304f64787ef5df3
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-06-23 10:15:52 +0100

    About page refactored into gallery and navbar file created + rendered in gallery

---
+++ project_frontend/src/components/Gallery.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ImageSlider from "./ui/ImageSlider";
+import { SliderData } from './ui/SliderData';
+import Navbar from './ui/Navbar';
+// import "../styles/Gallery.css";
+
+
+const Gallery = () => {
+    return (
+        <div className='gallery'>
+            <Navbar />
+            <h2 className='g-title'>Gallery</h2>
+                <ImageSlider slides={SliderData} className="slide-bar"/>
+                
+        </div>
+    )
+}
+
+export default Gallery;
---
+++ project_frontend/src/components/ui/Navbar.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+// import "../../styles/Navbar.css";
+import React from 'react';
+
+
+const Navbar = () => {
+
+    return (
+        <>
+          <div className="Nav">
+  
+            <div className='navbar'> 
+            <h1 className='nav-logo'>Three of Might</h1>
+        
+              <ul className='nav-ul'>         
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/gallery'>Gallery</Link></li>
+                <li><Link to='/characters'>Characters</Link></li>
+                <li><Link to='/sagas'>Sagas</Link></li>
+                <li><Link to='/techniques'>Techniques</Link></li>
+              </ul>
+            </div>
+          </div>
+        </>
+    );
+}
+
+export default Navbar;
