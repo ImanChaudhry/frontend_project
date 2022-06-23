66ada5727f374e6b24ce9f0da224889e939d870f
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-06-23 16:32:17 +0100

    Adjusted card media queries

--- project_frontend/src/components/ui/Navbar.js
+++ project_frontend/src/components/ui/Navbar.js
@@ -3,21 +3,21 @@ import "../../styles/Navbar.css";
 import React from 'react';
 
 
+
 const Navbar = () => {
 
     return (
         <>
-          <div className="Nav">
+        <div className="Nav">
   
-            <div className='navbar'> 
+          <div className='navbar'> 
             <h1 className='nav-logo'>Three of Might</h1>
-        
               <ul className='nav-ul'>         
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/gallery'>Gallery</Link></li>
-                <li><Link to='/characters'>Characters</Link></li>
-                <li><Link to='/sagas'>Sagas</Link></li>
-                <li><Link to='/techniques'>Techniques</Link></li>
+                <li className='nav-item'><Link to='/'>Home</Link></li>
+                <li className='nav-item'><Link to='/gallery'>Gallery</Link></li>
+                <li className='nav-item'><Link to='/characters'>Characters</Link></li>
+                <li className='nav-item'><Link to='/sagas'>Sagas</Link></li>
+                <li className='nav-item'><Link to='/techniques'>Techniques</Link></li>
               </ul>
             </div>
           </div>
